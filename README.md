dc4a1f737d09ea36d23b3d2115eaf61c7bf601fc
Bodhisatan <1813206474@qq.com>
2018-09-18 20:57:32 +0800

    change and add sth

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # LiteratureClockMail
+[中文文档](#中文文档)
+
+A script written by node.js to send email with Literature-Clock, based on work and idea by [Jaap Meijers](https://www.instructables.com/id/Literary-Clock-Made-From-E-reader/).
+Also Thanks for the picture source coming from another developer's [open-source-code](https://github.com/boramalper/literary-clock).
+
+### *preview first*
+![image][https://github.com/bodhisatan/LiteratureClockMail/preview.png]
+
+### *how to use it*
+* first you have to deploy the environment of node.js
+* then you should git clone or download the repository
+* after that you can use command 'npm install' to install the modules that requested
+* next you must open 'main.js' to modify some parameter
+* at last you can use 'node main.js' to run the script
+
+### *parameter settings*
+* if you are interested in the quick and dirty script, you can change the following settings to have a try
+* if you do not have a email account with SMTP service, you have to apply for it first
+```JavaScript
+let EmianService = "163";  //here you should input the factory that provides the email service, such as '163'
+
+let EamilAuth = {
+  user: "your email address",
+  pass: "your SMTP password"
+};
+
+let EmailFrom = '"you should input your nickname here" <you should input your email address here>';
+
+let EmailTo = "email address of recipient";
+
+let EmailSubject = "the email theme";
+```
+### 中文文档
+这是一个node.js写的小脚本，采用EJS模版引擎生成HTML，并将此HTML通过nodemailer发送给自己的小女友，定时功能采用node-schedule模块实现
+
+* 如何使用 
+    * 第一步：在本地配置node环境
+    * 第二步：将代码git clone或直接下载到本地
+    * 第三步：使用"npm install"命令安装依赖
+    * 第四步：修改main.js配置部分
+    * 第五步：使用"node main.js"运行脚本
+
+* 配置修改
+    * 如果你对此脚本有兴趣，欢迎修改了配置部分后将其运行于你的pc或者云服务器
+    * 如果你的邮箱尚未开通SMTP，请先开通再填写相应配置（关于如何开通，请自行百度/Google）
+```JavaScript
+let EmianService = "163";  //此处填写邮箱厂商，如163
+
+let EamilAuth = {
+  user: "你的邮箱",
+  pass: "SMTP密码"
+};
+
+let EmailFrom = '"发件人昵称" <你的邮箱地址>';
+
+let EmailTo = "接收人邮箱地址";
+
+let EmailSubject = "邮件主题";
+```
+*如果对你有所帮助，欢迎 star 或 fork ！*
+
--- main.js
+++ main.js
@@ -1,36 +1,30 @@
-const nodemailer = require("nodemailer"); //发送邮件的node插件
-const ejs = require("ejs"); //ejs模版引擎
-const fs = require("fs"); //文件读写
-const path = require("path"); //路径配置
-const schedule = require("node-schedule");//定时任务
+const nodemailer = require("nodemailer");
+const ejs = require("ejs");
+const fs = require("fs");
+const path = require("path");
+const schedule = require("node-schedule");
 
-require("./quoteMap");//引入全局变量
+require("./quoteMap");
 
-//发送者邮箱厂家
-let EmianService = "163";
+let EmianService = "163";  //here you should input the factory that provides the email service, such as '163'
 
-//发送者邮箱账户SMTP授权码
 let EamilAuth = {
-  user: "littlepumpkinhead@163.com",
-  pass: "wingman199939"
+  user: "your email address",
+  pass: "your SMTP password"
 };
 
-//发送者昵称与邮箱地址
-let EmailFrom = '"小姚同学" <littlepumpkinhead@163.com>';
+let EmailFrom = '"you should input your nickname here" <you should input your email address here>';
 
-//接收者邮箱地址
-let EmailTo = "1807792487@qq.com";
-//let EmailTo = "1813206474@qq.com";
+let EmailTo = "email address of recipient";
 
-//邮件主题
-let EmailSubject = "写给他最最最喜翻的野蛮女友";
+let EmailSubject = "the email theme";
 
-//Html数据初始化
+//initialize HtmlData
 let HtmlData = {};
 
 function getData() {
 
-  //文本信息
+  //text information
   let today = new Date();
   let hours = today.getHours();
   let minutes = today.getMinutes();
@@ -46,7 +40,7 @@ function getData() {
   console.log(textData);
   HtmlData.textData = textData;
 
-  //图片信息
+  //picture information
   hours = (hours - 10 + 24) % 24;
   minutes = minutes - (minutes % 10);
   const
@@ -70,7 +64,7 @@ function findClosest(A, T) {
   return A.reduce((prev, curr) => Math.abs(curr - T) < Math.abs(prev - T) ? curr : prev);
 }
 
-//随机选择时间一样的文本
+//select text at random
 function sample(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
@@ -98,15 +92,15 @@ function sendMail() {
     if (error) {
       return console.log(error);
       HtmlData = {};
-      getData(); //当登录、发送邮件失败，再次执行任务;
+      getData(); //after failing to send email, it will restart
     }
     console.log("Message sent: %s", info.messageId);
-    HtmlData = {}; //清除数据以便下次再次发送
+    HtmlData = {}; //clear data
   });
 }
 
-//schedule定时任务
-/*var rule = new schedule.RecurrenceRule();
+//schedule
+var rule = new schedule.RecurrenceRule();
 rule.dayOfWeek = [0, new schedule.Range(1, 6)];
 rule.hour = [0, new schedule.Range(1,23)];
 rule.minute = 50;
@@ -114,6 +108,4 @@ rule.second = 0;
 var j = schedule.scheduleJob(rule, function(){
   console.log("begin");
   getData();
-});*/
-
-getData();
+});
